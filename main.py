607e85050ed443b114c67e0c3774cceb77102cc9
tarasowski <dimitri+github@tarasowski.de>
2019-11-05 16:46:53 +0100

    customer-churn confusion matrix

--- main.py
+++ main.py
@@ -64,5 +64,67 @@ def accuracy(y_true, y_pred):
     return np.mean(y_true == y_pred)
 
 from sklearn.linear_model import LogisticRegression as LR
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier as RF
+from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.ensemble import GradientBoostingClassifier as GBC
+from sklearn.metrics import average_precision_score
 
 print('Logistic Regression \n', accuracy(y, run_cv(X, y, LR)))
+print('Gradient Boosting Classifier \n', accuracy(y, run_cv(X, y, GBC)))
+print('Support vector machines \n', accuracy(y, run_cv(X, y, SVC)))
+print('Random Forest \n', accuracy(y, run_cv(X, y, RF)))
+print('K-nearest-neighbors \n', accuracy(y, run_cv(X, y, KNN)))
+
+# Precision and recall
+# Confusion matrix is a way of visualizing predictions made by a classifier
+# x-axis shows the true class (if customer churned or not)
+# y-axis corresponds to the class predicted by the model
+
+#                   # condition true | condition false
+# predicton true     true positive   | false positive
+# prediction false   false negative  | true negative
+
+#                   spam             | ham
+# prediction spam   100              | 50
+# prediction ham    75               | 900
+
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+
+
+def draw_confusion_matrices(confusion_matrices, class_names):
+    class_names = class_names.tolist()
+
+    for cm in confusion_matrices:
+        classifier, cm = cm[0], cm[1]
+        print(cm)
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        cax = ax.matshow(cm)
+        plt.title(f'Confusion matrix for {classifier}')
+        fig.colorbar(cax)
+        ax.set_xticklabels([''] + class_names)
+        ax.set_yticklabels([''] + class_names)
+        plt.xlabel('Predicted')
+        plt.ylabel('True')
+        plt.show()
+
+y = np.array(y)
+class_names = np.unique(y)
+
+confusion_matrices = [
+        ('Support Vector Machines', confusion_matrix(y, run_cv(X,y,SVC))),
+        ('Random Forest', confusion_matrix(y, run_cv(X,y,RF))),
+        ('K-Nearest-Neighbors', confusion_matrix(y, run_cv(X,y,KNN))),
+        ('Gradient Boosting Classifier', confusion_matrix(y, run_cv(X,y,GBC))),
+        ('Logistic Regression', confusion_matrix(y, run_cv(X,y,LR))),
+        ]
+
+draw_confusion_matrices(confusion_matrices, class_names)
+
+# An important question to ask might be, when an individual churns, how often does my classifier predict that correctly?
+# This measurement is called `recall`
+    
